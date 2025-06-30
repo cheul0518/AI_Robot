f9b36a5cc8491f5f66a6d983b5c78e465d185ee5
Seungcheul Kim <cheul1987@gmail.com>
2025-06-30 11:30:47 +0900

    Update face_tracking.py and ObjectDetection.py

--- FaceTracking/face_tracking.py
+++ FaceTracking/face_tracking.py
@@ -16,9 +16,9 @@ from cvzone.SerialModule import SerialObject
 """
 
 # Load images
-background_img = cv2.imread('./Eye-Background.png', cv2.IMREAD_UNCHANGED)       # cv2.imread(): Load an image from a file. Returns a Numpy array
+background_img = cv2.imread('Images/Eye-Background.png', cv2.IMREAD_UNCHANGED)       # cv2.imread(): Load an image from a file. Returns a Numpy array
                                                                                 # cv2.IMREAD_COLOR(1): default. no alpha channel. 3 channels:BGR
-iris_img = cv2.imread('./Eye-Ball.png', cv2.IMREAD_UNCHANGED)                   # cv2.IMREAD_GRAYSCALE(0): load the image in grayscale. 1 channel
+iris_img = cv2.imread('Images/Eye-Ball.png', cv2.IMREAD_UNCHANGED)                   # cv2.IMREAD_GRAYSCALE(0): load the image in grayscale. 1 channel
                                                                                 # cv2.IMREAD_UNCHANGED(-1): load the image as-is, including the alpha channel(transparency). 4 channels:BGRA
 
 # Initialize camera
@@ -58,72 +58,75 @@ def overlay_iris(background, iris, x, y):
     for c in range(3):
         background[y:y+h, x:x+w, c] = alpha * iris[:, :, c] + (1-alpha) * background[y:y+h, x:x+w, c]
 
-# Create a named window and set it to full screen
-cv2.namedWindow('Overlay Result', cv2.WND_PROP_FULLSCREEN)              # cv2.WND_PROP_FULLSCREEN mode used with cv2.setWindowProperty() for full screen mode
-cv2.setWindowProperty('Overlay Result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-""" Why? cv2.namedWindow is necessary.
-You must create the window before you try to set its properties.
-cv2.setWindowProperty() can only modify an already existing window.
-If you skip cv2.namedWindow(), the window is automatically created when cv2.imshow() is called.
-But here's the catch: auto-created windows use default settings (e.g., cv2.WINDOW_AUTOSIZE) — and by then it's too late to change full screen or resizable mode.
-"""
+def main():
+    # Create a named window and set it to full screen
+    cv2.namedWindow('Overlay Result', cv2.WND_PROP_FULLSCREEN)              # cv2.WND_PROP_FULLSCREEN mode used with cv2.setWindowProperty() for full screen mode
+    cv2.setWindowProperty('Overlay Result', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    """ Why? cv2.namedWindow is necessary.
+    You must create the window before you try to set its properties.
+    cv2.setWindowProperty() can only modify an already existing window.
+    If you skip cv2.namedWindow(), the window is automatically created when cv2.imshow() is called.
+    But here's the catch: auto-created windows use default settings (e.g., cv2.WINDOW_AUTOSIZE) — and by then it's too late to change full screen or resizable mode.
+    """
 
-# Initialize iris position in the center
-iris_position = (325, 225)
+    # Initialize iris position in the center
+    iris_position = (325, 225)
 
-while True:
-    success, img = cap. read()              # Capture frame by frame
-                                            # success is true if the frame was read successfully. img is the actual image (frame) data
-    if not success:                         # exit if success is false
-        print('Failed to grab frame.')
-        break
+    while True:
+        success, img = cap. read()              # Capture frame by frame
+                                                # success is true if the frame was read successfully. img is the actual image (frame) data
+        if not success:                         # exit if success is false
+            print('Failed to grab frame.')
+            break
 
-    img = cv2.flip(img, 1)          # flipCode
-                                            #  0 : Vertical: Flips image up-down (over x-axis)
-                                            #  1 : Horizontal: Flips image left-right (over y-axis)
-                                            # -1 : Horizontal and vertical: over x-axis and y-axis
+        img = cv2.flip(img, 1)          # flipCode
+                                                #  0 : Vertical: Flips image up-down (over x-axis)
+                                                #  1 : Horizontal: Flips image left-right (over y-axis)
+                                                # -1 : Horizontal and vertical: over x-axis and y-axis
 
-    img, bboxs = detector.findFaces(img)    # findFaces returns "the image with annotations (like rectangles)", and "a list of bounding boxes" where faces are found.
+        img, bboxs = detector.findFaces(img)    # findFaces returns "the image with annotations (like rectangles)", and "a list of bounding boxes" where faces are found.
 
-    if bboxs:                               # In Python, an empty list evaluates to False
-        cx = bboxs[0]['center'][0]          # bboxs is a list of dictionaries. bbox is like {'id':0,'bbox':[x1,y1,w1,h1],'score':[confidence],'center':[cx1,cy1]}
+        if bboxs:                               # In Python, an empty list evaluates to False
+            cx = bboxs[0]['center'][0]          # bboxs is a list of dictionaries. bbox is like {'id':0,'bbox':[x1,y1,w1,h1],'score':[confidence],'center':[cx1,cy1]}
 
-        # Calculate the x-angle adjustment based on PID
-        resultX = int(xPID.update(cx))      # xPID.update(cx): calculates how far the face is from center(320) and returns a correction value
-        # Update iris position based on resultX
-        if resultX > 1:
-            iris_position = (400, 225)      # Move iris to the right
-        elif resultX < -1:
-            iris_position = (250, 225)      # Move iris to the left
-        else:                               # iris at center
-            iris_position = (325, 225)
+            # Calculate the x-angle adjustment based on PID
+            resultX = int(xPID.update(cx))      # xPID.update(cx): calculates how far the face is from center(320) and returns a correction value
+            # Update iris position based on resultX
+            if resultX > 1:
+                iris_position = (400, 225)      # Move iris to the right
+            elif resultX < -1:
+                iris_position = (250, 225)      # Move iris to the left
+            else:                               # iris at center
+                iris_position = (325, 225)
 
-        """ Arduino component - to be replaced by a Raspberry Pi in future implementations.
-        # Control the servo based on xAngle
-        if abs(resultX) > center_threshold:
-            xAngle += resultX
-        arduino.sendData([180, 0, xAngle])
-        """
-    else:
-        print('No face detected.')
+            """ Arduino component - to be replaced by a Raspberry Pi in future implementations.
+            # Control the servo based on xAngle
+            if abs(resultX) > center_threshold:
+                xAngle += resultX
+            arduino.sendData([180, 0, xAngle])
+            """
+        else:
+            print('No face detected.')
 
-    # Overlay the iris on the background image
-    background_with_iris = background_img.copy()
-    overlay_iris(background_with_iris, iris_img, iris_position[0], iris_position[1])
+        # Overlay the iris on the background image
+        background_with_iris = background_img.copy()
+        overlay_iris(background_with_iris, iris_img, iris_position[0], iris_position[1])
 
-    cv2.imshow('img', img)                                  # Display the resulting frame in a window titled "Image"
-    cv2.imshow('Overlay Result', background_with_iris)
-    cv2.moveWindow('Overlay Result', 0, 0)            # cv2.moveWindow(window_name, x, y). It moves an OpenCV window to a specific position on your screen.
-                                                                    # window_name: Name of the window. It must match what you passed to cv2.imshow()
-                                                                    # x: Distance in pixels from the left edge of screen. y: Distance in pixels from the top edge of screen.
+        cv2.imshow('img', img)                                  # Display the resulting frame in a window titled "Image"
+        cv2.imshow('Overlay Result', background_with_iris)
+        cv2.moveWindow('Overlay Result', 0, 0)            # cv2.moveWindow(window_name, x, y). It moves an OpenCV window to a specific position on your screen.
+                                                                        # window_name: Name of the window. It must match what you passed to cv2.imshow()
+                                                                        # x: Distance in pixels from the left edge of screen. y: Distance in pixels from the top edge of screen.
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):                           # cv2.waitKey(1) waits 1 millisecond for a key press
-        break                                                       # & 0xFF: keep only the lowest 8 bits (ASCII) b/c cv2.waitkey() sometimes returns a value with extra bits
-                                                                    # key_raw  = 0x100071 -> Binary: 0001 0000 0000 0000 0111 0001
-                                                                    # 0xFF     = 0x0000FF -> Binary: 0000 0000 0000 0000 1111 1111
-                                                                    # Filtered =                     0000 0000 0000 0000 0111 0001 -> 0x71 = 113 = 'q'
-                                                                    # q key breaks the loop and ends the program
+        if cv2.waitKey(1) & 0xFF == ord('q'):                           # cv2.waitKey(1) waits 1 millisecond for a key press
+            break                                                       # & 0xFF: keep only the lowest 8 bits (ASCII) b/c cv2.waitkey() sometimes returns a value with extra bits
+                                                                        # key_raw  = 0x100071 -> Binary: 0001 0000 0000 0000 0111 0001
+                                                                        # 0xFF     = 0x0000FF -> Binary: 0000 0000 0000 0000 1111 1111
+                                                                        # Filtered =                     0000 0000 0000 0000 0111 0001 -> 0x71 = 113 = 'q'
+                                                                        # q key breaks the loop and ends the program
 
-cap.release()                                                       # Releases the camera
-cv2.destroyAllWindows()                                             # Closes any OpenCV windows (e.g. the "Image" window.)
+    cap.release()                                                       # Releases the camera
+    cv2.destroyAllWindows()                                             # Closes any OpenCV windows (e.g. the "Image" window.)
 
+if __name__=='__main__':
+    main()
--- ObjectDetection/ObjectDetection.py
+++ ObjectDetection/ObjectDetection.py
@@ -36,100 +36,104 @@ def play_audio(audio_source):
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
-# Load YOLO model
-model = YOLO('yolov10n.pt')     # Loads the YOLOv10 nano model
+def main():
+    # Load YOLO model
+    model = YOLO('yolov10n.pt')     # Loads the YOLOv10 nano model
 
-# Define target classes and corresponding images
-target_classes = {
-    'cell phone': 'Images/cell_phone.jpg',
-    'remote': 'Images/remote.jpg',
-    'bottle': 'Images/bottle.jpg',
-    'scissors': 'Images/scissors.jpg'
-}
+    # Define target classes and corresponding images
+    target_classes = {
+        'cell phone': 'Images/cell_phone.jpg',
+        'remote': 'Images/remote.jpg',
+        'bottle': 'Images/bottle.jpg',
+        'scissors': 'Images/scissors.jpg'
+    }
 
-# Webcam setup
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+    # Webcam setup
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
 
-while True:
-    success, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    if not success:
-        break
+    while True:
+        success, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        if not success:
+            break
 
-    # Perform inference on images or videos
-    # results = model('./Images/image1.jpg', show=True)     # show=True displays the image in a new window with bounding boxes drawn on detected objects.
-    # results = model('./Images/people.mp4', show=True)
+        # Perform inference on images or videos
+        # results = model('./Images/image1.jpg', show=True)     # show=True displays the image in a new window with bounding boxes drawn on detected objects.
+        # results = model('./Images/people.mp4', show=True)
 
-    # Perform inference on the current frame
-    results = model(frame, stream=True)
-    ''' Runs the YOLO model on the current frame.
-    stream(=True) allows for faster inference with a generator-style output that yields a results object for each frame.
-    Otherwise, it returns a result object directly. Generator is like 'loop-ready' box that gives you one result at a time when you iterate over it.
-    
-      Variable   |       Type         |   Description
-    -------------------------------------------------------------------------------------------------------------
-    results      |    generator       |   A generator yielding Results objects (one per frame)
-    r            |    Results         |   Holds all detections (boxes, class IDs, confidence, etc.) for one frame
-    r.boxes      |    Boxes           |   Container holding all individual bounding boxes in that frame
-    box          |    Boxes row       |   One detection (e.g., one cell phone or bottle)
-    '''
+        # Perform inference on the current frame
+        results = model(frame, stream=True)
+        ''' Runs the YOLO model on the current frame.
+        stream(=True) allows for faster inference with a generator-style output that yields a results object for each frame.
+        Otherwise, it returns a result object directly. Generator is like 'loop-ready' box that gives you one result at a time when you iterate over it.
+        
+          Variable   |       Type         |   Description
+        -------------------------------------------------------------------------------------------------------------
+        results      |    generator       |   A generator yielding Results objects (one per frame)
+        r            |    Results         |   Holds all detections (boxes, class IDs, confidence, etc.) for one frame
+        r.boxes      |    Boxes           |   Container holding all individual bounding boxes in that frame
+        box          |    Boxes row       |   One detection (e.g., one cell phone or bottle)
+        '''
 
-    for r in results:
-        for box in r.boxes:
-            # Get class ID and confidence
-            cls = int(box.cls[0])                       # box.cls -> a PyTorch tensor containing the class index of the detected object
-                                                        # Suppose YOLO detects a bottle, and the class ID for 'bottle' is 39. Then box.cls returns tensor([39.])
-                                                        # int(box.cls[0]) - int(39.) = 39. YOLO runs on PyTorch, and everything is returned as tensors by default. even if there's just one value.
-            class_name = model.names[cls]
-            confidence = box.conf[0]
+        for r in results:
+            for box in r.boxes:
+                # Get class ID and confidence
+                cls = int(box.cls[0])                       # box.cls -> a PyTorch tensor containing the class index of the detected object
+                                                            # Suppose YOLO detects a bottle, and the class ID for 'bottle' is 39. Then box.cls returns tensor([39.])
+                                                            # int(box.cls[0]) - int(39.) = 39. YOLO runs on PyTorch, and everything is returned as tensors by default. even if there's just one value.
+                class_name = model.names[cls]
+                confidence = box.conf[0]
 
-            # Check if detected class is in the target classes and confidence is above 0.4
-            if class_name.lower() in target_classes and confidence > 0.4:
-                # Get bounding box coordinates
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                '''box.xyxy: a Pytorch tensor containing the bounding box coordinates of the detected object: [x1, y1, x2, y2]
-                x1, y1 = top-left corner of the box. x2, y2 = bottom-right corner of the box
-                This is called the 'XYXY format'. and it defines a rectangle on the image.
-                Even though this is one box, YOLO keeps the coordinates in a 2D tensor like this:
-                box.xyxy -> tensor([[x1,y1,x2,y2]]), shape(box.xyxy) = (1,4), shape(box.xyxy[0])=(4,)
-                
-                map(function, list) applies the function to every item in the list, one by one
-                box.xyxy[0] is something like tensor([150.5, 200.0, 300.9, 400.2)
-                map(int, ...) = convert every float to an integer: [150, 200, 300, 400]
-                x1, y1, x2, y2 = ... unpacks the 4 numbers 
-                '''
+                # Check if detected class is in the target classes and confidence is above 0.4
+                if class_name.lower() in target_classes and confidence > 0.4:
+                    # Get bounding box coordinates
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    '''box.xyxy: a Pytorch tensor containing the bounding box coordinates of the detected object: [x1, y1, x2, y2]
+                    x1, y1 = top-left corner of the box. x2, y2 = bottom-right corner of the box
+                    This is called the 'XYXY format'. and it defines a rectangle on the image.
+                    Even though this is one box, YOLO keeps the coordinates in a 2D tensor like this:
+                    box.xyxy -> tensor([[x1,y1,x2,y2]]), shape(box.xyxy) = (1,4), shape(box.xyxy[0])=(4,)
+                    
+                    map(function, list) applies the function to every item in the list, one by one
+                    box.xyxy[0] is something like tensor([150.5, 200.0, 300.9, 400.2)
+                    map(int, ...) = convert every float to an integer: [150, 200, 300, 400]
+                    x1, y1, x2, y2 = ... unpacks the 4 numbers 
+                    '''
 
-                # Draw corner rectangle
-                cvzone.cornerRect(frame, (x1, y1, x2-x1, y2-y1), l=30, t=5, rt=0, colorR=(0,255,0))
-                # Add label with confidence using cvzone
-                cvzone.putTextRect(frame, f'{class_name}{confidence:.2f}',(x1,y1-20))
+                    # Draw corner rectangle
+                    cvzone.cornerRect(frame, (x1, y1, x2-x1, y2-y1), l=30, t=5, rt=0, colorR=(0,255,0))
+                    # Add label with confidence using cvzone
+                    cvzone.putTextRect(frame, f'{class_name}{confidence:.2f}',(x1,y1-20))
 
-                text_to_speech(f'I found a {class_name}')
-                print(f'Detected: {class_name} with confidence {confidence:.2f}')
+                    text_to_speech(f'I found a {class_name}')
+                    print(f'Detected: {class_name} with confidence {confidence:.2f}')
 
-                # Display corresponding image for the detected object on the second monitor
-                img_path = target_classes[class_name.lower()]
-                if os.path.exists(img_path):
-                    detected_img = cv2.imread(img_path)
+                    # Display corresponding image for the detected object on the second monitor
+                    img_path = target_classes[class_name.lower()]
+                    if os.path.exists(img_path):
+                        detected_img = cv2.imread(img_path)
 
 
-                    # cv2.namedWindow('Detected Image', cv2.WND_PROP_FULLSCREEN)
-                    # cv2.setWindowProperty('Detected Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-                    # cv2.moveWindow('Detected Image', 0, 0)
-                    cv2.namedWindow('Detected Image', cv2.WINDOW_NORMAL)
-                    cv2.resizeWindow('Detected Image', 600, 400)
-                    # cv2.moveWindow('Detected Image', 640, 0)
-                    cv2.imshow('Detected Image', detected_img)
+                        # cv2.namedWindow('Detected Image', cv2.WND_PROP_FULLSCREEN)
+                        # cv2.setWindowProperty('Detected Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                        # cv2.moveWindow('Detected Image', 0, 0)
+                        cv2.namedWindow('Detected Image', cv2.WINDOW_NORMAL)
+                        cv2.resizeWindow('Detected Image', 600, 400)
+                        # cv2.moveWindow('Detected Image', 640, 0)
+                        cv2.imshow('Detected Image', detected_img)
 
-    # Display the updated frame with corner rectangles
-    cv2.imshow('Camera', frame)
+        # Display the updated frame with corner rectangles
+        cv2.imshow('Camera', frame)
 
-    # Press 'q' to quit the program
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Press 'q' to quit the program
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# Release resources
-cap.release()
-cv2.destroyAllWindows()
+    # Release resources
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
